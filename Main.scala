88dbb567a264e700890130480a43275c5c951edc
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-03-18 11:41:50 +0100

    Unwrap ClassLoader

--- Main.scala
+++ Main.scala
@@ -6,8 +6,13 @@ import cats.effect.IOApp
 import org.scalasteward.core.application.Cli
 import org.scalasteward.core.application.Context
 
+import java.net.URLClassLoader
+import java.lang.ClassLoader
+
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
+    unwrapClassLoader(getClass.getClassLoader, "")
+
     val tmp = Files.createTempDirectory("temp-steward")
     val a = Cli.Args(
       workspace = tmp,
@@ -22,5 +27,20 @@ object Main extends IOApp {
       doNotFork = true
     )
     Context.step0[IO](a).use(_.stewardAlg.runF)
+  }
+
+  def unwrapClassLoader(classLoader: ClassLoader, shift: String): Unit = {
+    println(shift + classLoader.getClass().getCanonicalName() + ":")
+    classLoader match {
+      case urlClassLoader: URLClassLoader =>
+        println(shift + "  urls:")
+        urlClassLoader.getURLs().foreach(url => println(shift + "    " + url))
+      case _ => ()
+    }
+    val parent = classLoader.getParent()
+    if (parent != null) {
+      println(shift + "  parent:")
+      unwrapClassLoader(parent, shift + "    ")
+    }
   }
 }
